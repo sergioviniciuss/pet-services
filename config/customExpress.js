726bb7d15e5a2e9ef4e740f0e5d58e9209d9ae04
sergioviniciuss <sergioviniciuss@gmail.com>
2021-01-21 22:35:29 +0100

    Feat(body-parser): Includes it in the configs so we can parse the requests properly

--- config/customExpress.js
+++ config/customExpress.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const consign = require("consign");
 
 module.exports = () => {
   const app = express();
 
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.json());
   consign().include("controllers").into(app);
 
   return app;
