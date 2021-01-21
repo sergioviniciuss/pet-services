113ca6c921c95ddaef8e887557ddc1d9c88235e0
sergioviniciuss <sergioviniciuss@gmail.com>
2021-01-21 22:34:28 +0100

    Feat(services): Includes post request

--- controllers/services.js
+++ controllers/services.js
@@ -1,5 +1,10 @@
 module.exports = (app) => {
   app.get("/services", (req, res) => {
-    res.send("You're in services route");
+    res.send("You're in services route and making a GET request");
+  });
+
+  app.post("/services", (req, res) => {
+    console.log(req.body);
+    res.send("You're in services route and making a POST request");
   });
 };
